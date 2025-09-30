4c0fe40eafcd2babd147be0fb7fb766fd3abad4f
Jamik <dzamikhidirov8@gmail.com>
2025-10-01 01:59:51 +0300

    Добалена состояния Onbording в DataStore

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,48 +1,62 @@
 package com.example.weatherapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.example.weatherapp.modal.AppDataStoreManager
 import com.example.weatherapp.ui.theme.WeatherAppTheme
+import com.example.weatherapp.view.screens.HomeScreen
 import com.example.weatherapp.view.screens.OnbordingScreen
 import com.example.weatherapp.view.screens.SplashScreen
+import com.example.weatherapp.viewmodal.MainViewModal
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: MainViewModal by viewModels()
+
+    @SuppressLint("UnrememberedMutableState")
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-        val splashScreen = installSplashScreen()
-
-        var keepSplashScreen = true
-        splashScreen.setKeepOnScreenCondition { keepSplashScreen }
-
         setContent {
             WeatherAppTheme {
-                var showSplash by remember { mutableStateOf(true) }
-                if (showSplash) {
-                    SplashScreen(onTimeout = { showSplash = false })
+                // Следим за состоянием из ViewModel
+                val hasUserSeenOnboarding by viewModel.isOnBordingCompleted.collectAsStateWithLifecycle()
+
+                var showSplashScreen by remember {mutableStateOf(true)}
+
+
+                // Показываем соответствующий экран
+                if (hasUserSeenOnboarding) {
+                    if (showSplashScreen){
+                        SplashScreen { showSplashScreen = false }
+                    }
+                    else{
+                        HomeScreen()
+                    }
                 } else {
                     OnbordingScreen {
-
+                        // Когда пользователь завершает онбординг, сохраняем это
+                        viewModel.completeOnBording()
                     }
-                }
-
-                // Отмечаем приложение готовым сразу после первого рендера
-                LaunchedEffect(Unit) {
-                    keepSplashScreen = false
                 }
             }
         }
     }
 }
-
-
-
---
+++ app/src/main/java/com/example/weatherapp/di/DataStoreModule.kt
@@ -0,0 +1,32 @@
+package com.example.weatherapp.di
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import com.example.weatherapp.modal.AppDataStoreManager
+import com.example.weatherapp.modal.settingsDataStore
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class) //Модуль буде жить за все время приложения
+object DataStoreModule {
+
+    @Provides
+    @Singleton
+    fun providerDataStore(@ApplicationContext context: Context): DataStore<Preferences>{
+        return context.settingsDataStore
+    }
+
+    @Provides
+    @Singleton
+    fun provideAppDataStoreManager(dataStore: DataStore<Preferences>): AppDataStoreManager {
+        return AppDataStoreManager(dataStore)
+    }
+
+}
