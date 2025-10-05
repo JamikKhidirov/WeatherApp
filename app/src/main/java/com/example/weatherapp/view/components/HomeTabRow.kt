4c7428bf97fe2564cee0cb7ddaa4c9598723ad9e
Jamik Khidirov <dzamikhidirov8@gmail.com>
2025-10-05 16:38:59 +0300

    Работа над HomeScreen

--- app/src/main/java/com/example/weatherapp/view/components/HomeTabRow.kt
+++ app/src/main/java/com/example/weatherapp/view/components/HomeTabRow.kt
@@ -1,13 +1,9 @@
 package com.example.weatherapp.view.components
 
-import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.animation.core.tween
+
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material3.ScrollableTabRow
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
 import androidx.compose.material3.TabRowDefaults
@@ -21,7 +17,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
